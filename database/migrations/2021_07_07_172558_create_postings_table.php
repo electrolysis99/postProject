95c16a6e3147eaaac4c491995f98aa586fb11194
electrolysis99 <63720572+electrolysis99@users.noreply.github.com>
2021-07-10 00:19:56 +0800

    Add files via upload

---
+++ database/migrations/2021_07_07_172558_create_postings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('content');
+            $table->string('meta_description');
+            $table->string('post_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postings');
+    }
+}
